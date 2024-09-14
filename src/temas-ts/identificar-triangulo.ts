eb15d8cb725ce284dc4b023c0002aab281cd1117
Miriam Del Rayo Aguayo López <miriamaglop20@gmail.com>
2024-09-13 20:55:31 -0600

    penultimo commit - Viernes 13 Septiembre

---
+++ src/temas-ts/identificar-triangulo.ts
@@ -0,0 +1,17 @@
+import { Distancia } from "./distancia-2-puntos";
+
+const tri:Distancia[]=[
+    {
+        pX1: 25,
+        pY1: 80,
+        pX2: 0,
+        pY2: 0,
+        dis: 0,
+        distanciaO: function (): void {
+            throw new Error("Function not implemented.");
+        },
+        imprimir: function (): void {
+            throw new Error("Function not implemented.");
+        }
+    }
+]
