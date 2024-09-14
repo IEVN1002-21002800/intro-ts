47235ffee31ef5c3e0cb023ac299ef9ae4328416
Miriam Del Rayo Aguayo López <miriamaglop20@gmail.com>
2024-09-14 10:26:03 -0600

    commit final - Sábado 14 Septiembre

--- src/temas-ts/distancia-2-puntos.ts
+++ src/temas-ts/distancia-2-puntos.ts
@@ -1,25 +1,31 @@
 export class Distancia{
-    pX1:number;
-    pY1:number;
-    pX2:number;
-    pY2:number;
+    protected pX1:number;
+    protected pY1:number;
+    protected pX2:number;
+    protected pY2:number;
     dis: number = 0;
+    dis2: number = 0;
+    
     constructor(pX1:number, pY1:number, pX2:number, pY2:number){
         this.pX1 = pX1;
         this.pY1 = pY1;
         this.pX2 = pX2;
         this.pY2 = pY2;
     }
-    distanciaO(){
-        this.dis = Math.sqrt(((this.pX2-this.pX1)**2) + ((this.pY2 - this.pY1)**2));
+    
+    distancia0(x1: number, y1:number, x2:number, y2:number): number{
+        this.dis = Math.sqrt(((x2-x1)**2) + ((y2 - y1)**2));
+        this.dis2 = Math.sqrt(((this.pX2-this.pX1)**2) + ((this.pY2 - this.pY1)**2));
+        return this.dis;
     }
 
     imprimir(){
         console.log(`Punto 1 es: (${this.pX1},${this.pY1}) Punto 2 es: (${this.pX2},${this.pY2})`)
-        console.log(`La distancia entre los 2 puntos es: ${this.dis}`)
+        console.log(`La distancia entre los 2 puntos es: ${this.dis2}`)
     }
 }
+
 let dis2:Distancia;
-dis2 = new Distancia(25,80,50,45)
-dis2.distanciaO();
-dis2.imprimir();
+dis2 = new Distancia(-4,-3,2,5)
+dis2.distancia0(-4,-3,2,5)
+dis2.imprimir()
--- src/temas-ts/identificar-triangulo.ts
+++ src/temas-ts/identificar-triangulo.ts
@@ -1,17 +1,46 @@
 import { Distancia } from "./distancia-2-puntos";
 
-const tri:Distancia[]=[
-    {
-        pX1: 25,
-        pY1: 80,
-        pX2: 0,
-        pY2: 0,
-        dis: 0,
-        distanciaO: function (): void {
-            throw new Error("Function not implemented.");
-        },
-        imprimir: function (): void {
-            throw new Error("Function not implemented.");
-        }
+class Triangulo extends Distancia{
+    private pX3: number;
+    private pY3: number;
+    private lado1:number = 0;
+    private lado2:number = 0;
+    private lado3:number = 0;
+
+    constructor(pX1:number, pY1:number, pX2:number, pY2:number, pX3:number, pY3:number){
+        super(pX1, pY1, pX2, pY2);
+        this.pX3 = pX3;
+        this.pY3 = pY3;
     }
-]
+
+    public Tri():boolean{
+        this.lado1 = this.distancia0(this.pX1, this.pY1, this.pX2, this.pY2); //Punto 1 a 2
+        this.lado2 = this.distancia0(this.pX2, this.pY2, this.pX3, this.pY3); //Punto 2 a 3
+        this.lado3 = this.distancia0(this.pX1, this.pY1, this.pX3, this.pY3); //Punto 1 a 3
+
+        return  this.lado1+this.lado2 > this.lado3 && 
+                this.lado2+this.lado3 > this.lado1 && 
+                this.lado1+this.lado3 > this.lado2;
+    }
+
+    public imprimirTri(): void {
+        if (this.Tri()) {
+            tri.imprimirT()
+            console.log('Los tres puntos forman un triángulo.');
+        } else {
+            tri.imprimirT()
+            console.log('Los tres puntos NO forman un triángulo.');
+        }
+    }
+
+    imprimirT(){
+        console.log(`Punto 1 es: (${this.pX1},${this.pY1}) Punto 2 es: (${this.pX2},${this.pY2}) Punto 3 es: (${this.pX3},${this.pY3})`)
+        console.log(`La distancia entre puntos 1 y 2 es: ${this.lado1}`)
+        console.log(`La distancia entre puntos 2 y 3 es: ${this.lado2}`)
+        console.log(`La distancia entre puntos 1 y 3 es: ${this.lado3}`)
+    }
+}
+
+let tri:Triangulo;
+tri = new Triangulo(-10,6,10,0,0,3)
+tri.imprimirTri();
